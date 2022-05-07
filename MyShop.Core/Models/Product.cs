c0d95ccaa3d45102e4850cc32ac617d6cc10762f
gitbhanu98 <97779388+gitbhanu98@users.noreply.github.com>
2022-05-07 12:19:08 +0530

    Product model and In Memory Product Repo is Built

---
+++ MyShop.Core/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    // By Default class will be private, we need to make this below class Public so that other classes/namespaces/projects can access this class
+    public class Product
+    {
+        public string Id { get; set; }
+        
+        //Limit the size of Name, display the Name of Product
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        //Adding the Range of the Price
+        [Range(0,10000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        //Adding Constructor
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+
+    
+}
---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,84 @@
+﻿
+//Added References in this project: System.Runtime.Caching, MyShop.Core. See References of this project.
+
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    class ProductRepository
+    {
+        //using the namespace Runtime.Caching for ObjectCache,MemoryCache
+
+        ObjectCache cache = MemoryCache.Default;
+
+        //Using the namespace MyShop.Core.Models to use Product Model
+        List<Product> products;
+        
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        //Commit method is created to save the list of products added by Customer and store in Cache
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+                Console.WriteLine("product updated");
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        public Product Find(string Id)
+        {
+            Product productToFind = products.Find(p => p.Id == Id);
+            if (productToFind != null)
+            {
+                return productToFind;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        
+        }
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+    }
+}
